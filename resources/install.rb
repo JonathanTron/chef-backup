1538229fb27ec156523fe355d7a3b76b7dd8078a
Jonathan Tron <jonathan@tron.name>
2014-06-10 14:42:39 +0200

    Update omnibus-backup package to 3.10.0 and bump version to 1.0.2

--- resources/install.rb
+++ resources/install.rb
@@ -1,5 +1,5 @@
 actions :install, :remove
 default_action :install
 
-attribute :url, kind_of: String, default: "https://github.com/JonathanTron/omnibus-backup/releases/download/3.7.0/omnibus-backup_3.7.0.20130725212503-1.ubuntu.12.04_amd64.deb"
-attribute :checksum, kind_of: String, default: "17b092ed7b126c4db2cc47b30091aaa3b503c25481c5b1e5b22f26e7a851c497"
+attribute :url, kind_of: String, default: "https://github.com/JonathanTron/omnibus-backup/releases/download/3.10.0/omnibus-backup_3.10.0+20140610122512-1_amd64.deb"
+attribute :checksum, kind_of: String, default: "57db8aac2200b5ba01f92d5d93bbc211c5f898538cb026f7430bca8c97710b68"
