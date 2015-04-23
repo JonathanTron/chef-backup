2e7bccfa4700f8ca8728c76efd370c6ac94f0c94
Jonathan Tron <jonathan@tron.name>
2015-04-23 17:35:37 +0200

    Fix wrong checksum for omnibus backup 3.11.0 and bump to 1.0.6

--- resources/install.rb
+++ resources/install.rb
@@ -2,4 +2,4 @@ actions :install, :remove
 default_action :install
 
 attribute :url, kind_of: String, default: "https://github.com/JonathanTron/omnibus-backup/releases/download/3.11.0/omnibus-backup_3.11.0-1_amd64.deb"
-attribute :checksum, kind_of: String, default: "679799db3ae06ceba2229ad12b966b3a7e68e5c78b8fe244fc17eaca3cf476e5"
+attribute :checksum, kind_of: String, default: "f16b740c2b2408797f81d590124cc36791fb5908e798374c95cc34b6632dc18c"
